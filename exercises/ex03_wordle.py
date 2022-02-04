5741b19968c01e5c7dd10ba2ab7e88dcf5b15919
Grace Hanly <97480998+ghanly@users.noreply.github.com>
2022-02-04 13:35:38 -0500

    ex 03 final

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -5,6 +5,7 @@ __author__ = "730460994"
 
 def contains_char(guess: str, single_char: str) -> bool: 
     """Indicates if the single char exists in the guessed word."""
+    # creating a function to call when I want to evaluate if the character exists in the secret word or not
     assert len(single_char) == 1
     idx_check: int = 0
     while idx_check < len(guess):  
@@ -28,6 +29,7 @@ def contains_char(guess: str, single_char: str) -> bool:
 
 def emojified(guess: str, secret_word: str) -> str:
     """Returns an emoji string that correlates to the accuracy of the guess."""
+    # creating a function to call when I want to print the emoji result of the comparison of the guessed word to the secret word
     assert len(guess) == len(secret_word)
     emoji_result: str = "" 
     idx_check: int = 0
@@ -48,6 +50,7 @@ def emojified(guess: str, secret_word: str) -> str:
 
 def input_guess(expected_len: int) -> str: 
     """Prompts the user for a guess that is the expected length."""
+    # creating a function to call when I want to prompt the user for a guess that is the expected length
     guess: str = input(f"Enter a {expected_len} character word: ")  
     while len(guess) != expected_len:
         guess = input(f"That wasn't {expected_len} chars! Try again: ")
@@ -56,10 +59,14 @@ def input_guess(expected_len: int) -> str:
 
 def main() -> None: 
     """The entrypoint of the program and main game loop."""
+    # creating a main function to tie in the previous 3 functions
     secret_word: str = "codes"
+    # establishing what the secret word is as a variable
     max_turns: int = 6
     current_turn: int = 1
+    # creating a counter to only allow 6 turns
     won: bool = False
+    # creating a bool variable to act as a 'switch' that will allow the while loop to iterate when the player has not yet won
     while current_turn <= max_turns and not won: 
         print(f"=== Turn {current_turn}/6 ===")
         guess: str = input_guess(len(secret_word))
@@ -69,7 +76,8 @@ def main() -> None:
             won = True
         else: 
             current_turn = current_turn + 1
-            if not won and current_turn > max_turns:        
+            if not won and current_turn > max_turns:   
+                # since not yet won and after turn 6, I want to print this result     
                 print("X/6 - Sorry, try again tomorrow!")
 
 
